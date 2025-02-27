ee4768ee2c53d73dabd313110aca79abbc8a7833
Jakob Langtry <jjl4287@g.rit.edu>
2025-02-27 03:28:42 -0500

    Remove public directory files and clean up project structure

--- public/env.js
+++
@@ -1,7 +0,0 @@
-// Client-side environment variables
-const ENV = {
-    OPENWEATHERMAP_API_KEY: '5986f06fe95adb5a97c906eb96bd98dc' // This will be populated by the server
-};
-
-// Expose the environment to the window
-window.ENV = ENV; 
